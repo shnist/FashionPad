59314f307fcdd25e0f4d6b31de9046fb1199f727
Aaron Faber <shnist@btinternet.com>
2012-03-18 22:35:55 +0000

    on and off do not work on jquery mobile event handlers, modified accordingly

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -94,8 +94,8 @@
                 $('.navigation-bar, .navigation-bar-top').hide();
                 
                 // Show/hide navigation on a tap
-				$('.navigation-button').off('tap');
-                $('.navigation-button').on('tap', function() {
+				$('.navigation-button').unbind('tap');
+                $('.navigation-button').tap(function() {
                     $('.navigation-bar, .navigation-bar-top').toggle('fast', function() {
                     });
                 });    
